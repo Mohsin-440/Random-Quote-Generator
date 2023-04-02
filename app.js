0797ec3523ecd2a3f295426bc05fbc5f87a2c595
Mohsin_Ali <81881842+Mohsin-440@users.noreply.github.com>
2023-04-03 01:08:18 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -19,10 +19,9 @@ const complete = () => {
 // Get Quote From API
 async function getQuote() {
     loading();
-    const proxyUrl = "https://cors-anywhere.herokuapp.com/";
-    const apiUrl = "http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json";
+    const apiUrl = "http://localhost:5000/users";
   try {
-    const response = await fetch(proxyUrl + apiUrl);
+    const response = await fetch(apiUrl);
     const data = await response.json();
 
     // If author is blank, add unknown
